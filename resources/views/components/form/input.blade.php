5c62de66927179c3044da34f961ad1e9235856cc
Arshad A. <hi@ar5.in>
2025-04-08 23:50:23 +0530

    Moved logic to populate form inputs to input.blade.php

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,6 +1,12 @@
-@props(['type'=>'text', 'value'=>''])
+@props(['type'=>'text', 'value'=>'', 'name'=>'', 'options' => []])
 @if($type === 'textarea')
-<textarea {{ $attributes->merge(['class' => 'px-3 py-1.5 border-1 border-slate-500 rounded-lg text-primary']) }}>{{ $value }}</textarea>
+<textarea name="{{ $name }}" {{ $attributes->merge(['class' => 'px-3 py-1.5 border-1 border-slate-500 rounded-lg text-primary']) }}>{{ $value }}</textarea>
+@elseif($type === 'selection')
+    <select name="{{ $name }}" {{ $attributes->merge(['class' => 'px-3 py-1.5 border-1 border-slate-500 rounded-lg text-primary']) }}>
+        @foreach($options as $optionValue => $text)
+        <option value="{{ $optionValue }}" {{ old($name, $value) === (string)$optionValue ? 'selected' : '' }}>{{ $text }}</option>
+        @endforeach
+    </select>
 @else
-<input type="{{ $type }}" value="{{ $value }}" {{ $attributes->merge(['class' => 'px-3 py-1.5 border-1 border-slate-500 rounded-lg text-primary']) }}>
+<input name="{{ $name }}" type="{{ $type }}" value="{{ old($name, $value) }}" {{ $attributes->merge(['class' => 'px-3 py-1.5 border-1 border-slate-500 rounded-lg text-primary']) }}>
 @endif
