935950b5d4ee60eaee7118db619ca2066d135309
Arshad A. <hi@ar5.in>
2025-04-21 23:30:15 +0530

    Minor refactoring

--- resources/js/Pages/Jobs/Show.jsx
+++ resources/js/Pages/Jobs/Show.jsx
@@ -5,8 +5,8 @@ const Show = ({ job }) => {
     return <Page heading={`${job.title} (${job.employer.name})`}>
         <Tags tags={job.tags} />
         <p className="text-xl">
-            Based in <strong class="text-primary">{job.location}</strong>,
-            compensates <strong class="text-primary">{job.compensation}</strong>
+            Based in <strong className="text-primary">{job.location}</strong>,
+            compensates <strong className="text-primary">{job.compensation}</strong>
         </p>
     </Page>
 }
