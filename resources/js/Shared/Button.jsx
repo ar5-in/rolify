1a389dee975454a2c3cf96cd73de2c15e148104b
Arshad A. <hi@ar5.in>
2025-05-02 18:58:59 +0530

    Added onClick prop to Button component

--- resources/js/Shared/Button.jsx
+++ resources/js/Shared/Button.jsx
@@ -1,14 +1,14 @@
 import {Link} from "@inertiajs/react";
 
-export default function Button({ label = 'button', type, href }) {
+export default function Button({ label = 'button', type, href, onClick = () => {} }) {
     if(type === 'link')
     {
         return (
-            <Link className="inline-block text-primary hover:bg-black/10 px-6 py-2 rounded-full text-lg font-bold transition-colors duration-300" href={href}>{label}</Link>
+            <Link className="inline-block text-primary hover:bg-black/10 px-6 py-2 rounded-full text-lg font-bold transition-colors duration-300" href={href} onClick={onClick.bind(this)}>{label}</Link>
         )
     }
 
     return (
-        <button className="inline-block bg-primary text-body-bg px-6 py-2 rounded-full text-lg font-bold cursor-pointer" type={type}>{label}</button>
+        <button className="inline-block bg-primary text-body-bg px-6 py-2 rounded-full text-lg font-bold cursor-pointer" type={type} onClick={onClick.bind(this)}>{label}</button>
     )
 }
