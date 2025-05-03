0fccd833f0d4a55ba4a509a64e0770e9097c8d99
Arshad A. <hi@ar5.in>
2025-05-04 01:39:17 +0530

    Added state to filter jobs received as props to clear jobs from the UI that have been removed from saved jobs

--- resources/js/Pages/Jobs/Saved/Index.jsx
+++ resources/js/Pages/Jobs/Saved/Index.jsx
@@ -1,7 +1,16 @@
 import JobListings from "@/Shared/JobListings.jsx";
+import {useEffect, useState} from "react";
+import {useSavedJobs} from "@/Shared/SavedJobs/SavedJobsContext.jsx";
 
 export default function Index({jobs}) {
+    const [filteredJobs, setFilteredJobs] = useState(jobs);
+    const {savedJobs} = useSavedJobs();
+
+    useEffect(() => {
+        setFilteredJobs(fjs => fjs.filter(fj => savedJobs.some(sjId => fj.id === sjId)));
+    }, [savedJobs]);
+
     return (
-        <JobListings title="Saved Jobs" jobs={jobs} displayAs="list" withSorting count={jobs.length} />
+        <JobListings title="Saved Jobs" jobs={filteredJobs} displayAs="list" withSorting count={filteredJobs.length} />
     )
 }
