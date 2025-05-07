72806cc9a281e990883e9a52f5a3db022fee4444
Arshad A. <hi@ar5.in>
2025-05-08 01:21:51 +0530

    added the option to add controls before heading, to include the toggle save job button before the heading

--- resources/js/Shared/Page.jsx
+++ resources/js/Shared/Page.jsx
@@ -1,8 +1,11 @@
 import PageHeading from "@/Shared/PageHeading.jsx";
 
-export default function Page({heading, children}) {
+export default function Page({heading, children, topControls}) {
     return <article>
-        <PageHeading>{heading}</PageHeading>
+        <div className="flex items-center">
+            {topControls}
+            <PageHeading>{heading}</PageHeading>
+        </div>
         <section className="m-5 space-y-5">{children}</section>
     </article>
 }
