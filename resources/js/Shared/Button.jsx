ea2de678c53dc776776fc0948c1a7d95330e5b15
Arshad A. <hi@ar5.in>
2025-05-11 21:18:42 +0530

    feature: add new variant to button

--- resources/js/Shared/Button.jsx
+++ resources/js/Shared/Button.jsx
@@ -2,18 +2,24 @@ import {Link} from "@inertiajs/react";
 
 export default function Button({ label = 'Button', type, variant, href, onClick = () => {} }) {
 
-    const classes = variant === 'alternate' || type === 'link'
-        ? 'inline-block text-primary hover:bg-black/10 px-6 py-2 rounded-full text-lg font-bold cursor-pointer transition-colors duration-300'
-        : 'inline-block bg-primary text-body-bg px-6 py-2 rounded-full text-lg font-bold cursor-pointer';
+    const getClasses = (variant) => {
+        const classes = {
+            'default': 'inline-block bg-primary text-body-bg px-6 py-2 rounded-full text-lg font-bold cursor-pointer',
+            'alternate': 'inline-block text-primary hover:bg-black/10 px-6 py-2 rounded-full text-lg font-bold cursor-pointer transition-colors duration-300',
+            'danger': 'inline-block ml-auto text-red-600 hover:bg-black/10 px-6 py-2 rounded-full text-lg font-bold cursor-pointer transition-colors duration-300'
+        }
+
+        return classes[variant] ?? classes['default'];
+    }
 
     if(type === 'link')
     {
         return (
-            <Link className={classes} href={href} onClick={onClick.bind(this)}>{label}</Link>
+            <Link className={getClasses(variant)} href={href} onClick={onClick.bind(this)}>{label}</Link>
         )
     }
 
     return (
-        <button className={classes} type={type} onClick={onClick.bind(this)}>{label}</button>
+        <button className={getClasses(variant)} type={type} onClick={onClick.bind(this)}>{label}</button>
     )
 }
