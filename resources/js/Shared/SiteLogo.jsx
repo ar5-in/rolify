9a8330748686b39fed6688bdf72f99b537365c39
Arshad A. <hi@ar5.in>
2025-07-21 22:54:19 +0530

    feature: branding update

--- resources/js/Shared/SiteLogo.jsx
+++ resources/js/Shared/SiteLogo.jsx
@@ -2,6 +2,6 @@ import {Link} from "@inertiajs/react";
 
 export default function SiteLogo() {
     return <Link className="flex items-center px-7" href="/">
-        <img src={new URL(`/resources/images/logo.svg`, import.meta.url).href} alt=""/>
+        <img className="h-8 w-auto" src={new URL(`/resources/images/logo.svg`, import.meta.url).href} alt=""/>
     </Link>
 }
