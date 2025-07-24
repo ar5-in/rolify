a5deb26d572320d7dd360780916060fff5336200
Arshad A. <hi@ar5.in>
2025-07-24 16:16:26 +0530

    fixes: - title does not update (removed title tag from app template and added head component) - show vertical scrollbar always

--- resources/js/Pages/Jobs/Index.jsx
+++ resources/js/Pages/Jobs/Index.jsx
@@ -1,8 +1,9 @@
-import CardJob from "@/Shared/CardJob.jsx";
 import JobListings from "@/Shared/JobListings.jsx";
+import {Head} from "@inertiajs/react";
 
 const Index = ({ jobs, featuredJobs }) => {
     return <>
+        <Head title="Get Hired! Find your job" />
         <JobListings title="Featured Jobs" jobs={featuredJobs} displayAs="list" />
         <JobListings title="Recommended Jobs" jobs={jobs} displayAs="grid" withSorting count={jobs.length} />
     </>
