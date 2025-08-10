2d8330043284558955f0955bc986164e8ba05557
Arshad A. <hi@ar5.in>
2025-08-10 19:34:42 +0530

    fix: the 'featured jobs' listings section appears with no jobs

--- resources/js/Pages/Jobs/Index.jsx
+++ resources/js/Pages/Jobs/Index.jsx
@@ -4,7 +4,7 @@ import {Head} from "@inertiajs/react";
 const Index = ({ jobs, featuredJobs }) => {
     return <>
         <Head title="Get Hired! Find your job on Rolify" />
-        <JobListings title="Featured Jobs" jobs={featuredJobs} displayAs="list" scrollable />
+        {featuredJobs.length > 0 ? <JobListings title="Featured Jobs" jobs={featuredJobs} displayAs="list" scrollable /> : null}
         <JobListings title="Recommended Jobs" jobs={jobs} displayAs="grid" withSorting count={jobs.length} />
     </>
 }
