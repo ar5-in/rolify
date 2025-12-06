d4b5a42d6c545b695961e88bad8283216dd2dcb3
Arshad Ansari <hi@ar5.in>
2025-12-06 19:23:00 +0000

    fix: throws 500 when viewing created jobs page when user has no employers

--- app/Http/Controllers/CreatedJobsController.php
+++ app/Http/Controllers/CreatedJobsController.php
@@ -10,13 +10,8 @@ class CreatedJobsController extends Controller
 {
     function __invoke()
     {
-        $jobs = Job::query()
-            ->with('employer', 'tags')
-            ->whereBelongsTo(auth()->user()->employers)
-            ->get();
-
         return Inertia::render('Jobs/Created/Index')
-            ->with('jobs', $jobs->map(function($job) {
+            ->with('jobs', Job::createdBy(auth()->user())->map(function($job) {
                 return array_merge($job->toArray(), [
                     'authUser' => [
                         'can' => [
