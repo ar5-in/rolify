6525f696c5512c6db0bc23da42c8ce3d56b3aa87
Arshad Ansari <hi@ar5.in>
2026-01-15 15:20:08 +0530

    feature: paginate jobs
    * refactor: convert jobs response to include api response for jobs, employers and tags
    * chore: package-lock.json updated
    * feat: implement pagination component
    * refactor: move pagination component to separate file

--- resources/js/Pages/Tags/Index.jsx
+++ resources/js/Pages/Tags/Index.jsx
@@ -2,6 +2,6 @@ import JobListings from "@/Shared/JobListings.jsx";
 
 export default function Index({tag, jobs}) {
     return (
-        <JobListings title={`Jobs tagged '${tag.name}'`} jobs={jobs} displayAs="list" withSorting count={jobs.length} />
+        <JobListings title={`Jobs tagged '${tag.name}'`} jobs={jobs.data} displayAs="list" withSorting count={jobs.meta.total} paginationMeta={jobs.meta} />
     )
 }
