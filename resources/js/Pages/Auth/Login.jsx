04dee6ca02eb3babc3f168373aa6b51c21257790
Arshad Ansari <hi@ar5.in>
2026-01-19 17:15:34 +0000

    refactor: improve login and register page structure

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -5,6 +5,9 @@ import RequestForm from "@/Shared/RequestForm.jsx";
 import {router} from "@inertiajs/react";
 import {useAddNotification} from "@/Shared/Notifications/NotificationsContext.jsx";
 import FormAction from "@/Shared/Form/FormAction.jsx";
+import Card from "../../Shared/Card.jsx";
+import CardFooter from "../../Shared/CardFooter.jsx";
+import CardHead from "../../Shared/CardHead.jsx";
 
 export default function Login({}) {
     const addNotification = useAddNotification();
@@ -13,25 +16,33 @@ export default function Login({}) {
         const user = response.data && response.data.user ? response.data.user : null;
         const redirectTo = response.data && response.data.intended ? response.data.intended : '/';
         const message = user ? `Welcome ${user.name}. Logged successfully` : "Logged in successfully";
-        addNotification({message:message, type:"success"});
+        addNotification({message: message, type: "success"});
         router.visit(redirectTo);
     }
 
     return (
-        <Page heading="Login">
-            <RequestForm action="/login" method="post" onResolve={handleResolve.bind(this)}>
+        <div className={`mx-auto max-w-xl`}>
+            <Page heading="Login">
 
-                <FormControl label="Email" name="email" type="email"
-                             placeholder="Email Address" />
+                <RequestForm wide action="/login" method="post" onResolve={handleResolve.bind(this)}>
+                    <Card>
+                        <div className={`p-3 bg-transparent border border-black/10 rounded-xl`}>
+                            <FormControl label="Email" name="email" type="email"
+                                         placeholder="Email Address"/>
 
-                <FormControl label="Password" name="password" type="password"
-                             placeholder="Password" />
+                            <FormControl label="Password" name="password" type="password"
+                                         placeholder="Password"/>
+                        </div>
+                        <div className={`px-3 py-5`}>
+                            <FormActionGroup>
+                                <FormAction label="Login"/>
+                                <FormAction type="link" variant="alternate" label="Register" href="/register"/>
+                            </FormActionGroup>
+                        </div>
+                    </Card>
+                </RequestForm>
 
-                <FormActionGroup>
-                    <FormAction label="Login" />
-                    <FormAction type="link" variant="alternate" label="Register" href="/register" />
-                </FormActionGroup>
-            </RequestForm>
-        </Page>
+            </Page>
+        </div>
     )
 }
