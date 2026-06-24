2db8ba60e67689f132a46dd34e2e75b5850e9832
Arshad Ansari <hi@ar5.in>
2026-06-24 13:57:15 +0000

    feat: integrate Plunk mail transport and configure API settings

--- config/mail.php
+++ config/mail.php
@@ -37,6 +37,10 @@ return [
 
     'mailers' => [
 
+        'plunk' => [
+            'transport' => 'plunk',
+        ],
+
         'smtp' => [
             'transport' => 'smtp',
             'scheme' => env('MAIL_SCHEME'),
