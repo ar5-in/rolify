55a1cc2ac6514f9b35937c1ea13b8347e04e49a7
Arshad Ansari <hi@ar5.in>
2026-06-28 15:14:43 +0000

    feat: enhance job applications page with heading, date display, and reusable layout component

--- resources/js/Pages/JobApplications/Index.jsx
+++ resources/js/Pages/JobApplications/Index.jsx
@@ -1,4 +1,6 @@
-import {Link} from "@inertiajs/react";
+import {Head, Link} from "@inertiajs/react";
+import Page from "../../Shared/Page.jsx";
+import moment from "moment";
 
 export default function Index({jobApplications}) {
 
@@ -8,24 +10,27 @@ export default function Index({jobApplications}) {
         pending: 'bg-body-text'
     }
 
-    return <article>
-        <header className="flex justify-between mx-5 my-9">
-            <div className="flex space-x-4 items-center">
-                <h1 className="text-3xl font-bold text-primary">My Applications</h1>
-            </div>
-        </header>
-        {jobApplications.length > 0 ? (
-            <section className="mx-5">
-                {jobApplications.map(jobApplication => <div key={jobApplication.id} className="flex justify-between items-center border-t border-t-black/10 py-5">
-                    <Link key={jobApplication.id}
-                          href={`/jobs/${jobApplication.job.id}`}><strong className="text-primary">{jobApplication.job.title}</strong> ({jobApplication.job.employer.name})</Link>
+    return <>
+        <Head title={`My Applications`}/>
+        <Page heading={`My Applications`}>
+            {jobApplications.length > 0 ? (
+                <section className="mx-5">
+                    {jobApplications.map(jobApplication => <div key={jobApplication.id} className="flex justify-between items-center border-t border-t-black/10 py-5">
+                        <Link key={jobApplication.id}
+                              href={`/jobs/${jobApplication.job.id}`} className={`flex flex-col ${jobApplication.status === 'rejected' && 'line-through'}`}>
+                            <span>
+                                <strong className={jobApplication.status !== 'rejected' && 'text-primary'}>{jobApplication.job.title}</strong> ({jobApplication.job.employer.name})
+                            </span>
+                            <span>applied {moment(jobApplication.created_at).fromNow()}</span>
+                        </Link>
 
-                    <span
-                        className={ 'text-sm px-4 py-1 rounded-full text-white ' + statusClasses[jobApplication.status]}>{jobApplication.status}</span>
-                </div>)}
-            </section>
-        ) : (
-            <div className="p-5 border-t border-black/10 font-bold text-xl">Apply for your first job and track it's status here!</div>
-        )}
-    </article>
+                        <span
+                            className={ 'text-sm px-4 py-1 rounded-full text-white ' + statusClasses[jobApplication.status]}>{jobApplication.status}</span>
+                    </div>)}
+                </section>
+            ) : (
+                <div className="p-5 border-t border-black/10 font-bold text-xl">Apply for your first job and track it's status here!</div>
+            )}
+        </Page>
+    </>
 }
